3c01647e41bc4898ae7ff1c5f2989d9d9a7c445f
Your Name <you@example.com>
2026-07-26 21:41:39 +0700

    modify config next

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  compilerOptions: {
-    baseUrl: ".",
-    paths: {
-      "@/*": ["./src/*"],
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
